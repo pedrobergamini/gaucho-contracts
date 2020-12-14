798d724ee1a07f2ea1c344b5dd8102a74ce35694
pedrobergamini <pedrohgbergamini@gmail.com>
2020-12-14 14:20:38 -0300

    chore: updates migrations

--- migrations/1_migration.js
+++ migrations/1_migration.js
@@ -2,5 +2,7 @@ const GauchoFactory = artifacts.require('GauchoFactory');
 
 module.exports = function(deployer) {
   const masterChefAddress = '0xc2EdaD668740f1aA35E4D8f227fB8E17dcA888Cd';
-  deployer.deploy(GauchoFactory, masterChefAddress);
+  const sushiAddress = '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2';
+
+  deployer.deploy(GauchoFactory, masterChefAddress, sushiAddress);
 };
